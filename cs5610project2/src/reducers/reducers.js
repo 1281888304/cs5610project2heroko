93f876e0970ec6bb37de201ffc3ded9a5a37eea9
QINGHANG ZHANG <Qzhang9@mail.greenriver.edu>
2021-11-17 16:45:29 -0800

    delete something

--- cs5610project2/src/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import enemyReducer from "./enemyReducer";
-import gameReducer from "./gameReducer";
-
-export default combineReducers({
-    game: gameReducer,
-    enemyGame: enemyReducer,
-})
